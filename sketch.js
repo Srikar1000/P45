517dcf42a58c5d0c4cbf9cadbcf5625a782acf76
Mahesh pidikiti <nicoletardner@gmail.com>
2023-07-26 18:40:52 -0700

    finished product

--- sketch.js
+++ sketch.js
@@ -3,8 +3,7 @@ var bird, birdImg;
 var obstacle1, obstacle1Img, obstacleGroup,obstacle2;
 var obstacle3, obstacle2Img, obstacle3Img;
 
-//var gameState = "play";
-//var gameOver, gameOverImg;
+
 
 function preload(){
     createCanvas(1200,1200);
@@ -22,10 +21,10 @@ function setup() {
    cloud.addImage("cloud", cloudImg);
     bird = createSprite(20,100);
     bird.addImage("bird",birdImg)
-    bird.scale = 0.1;
+    bird.scale = 0.09;
     obstacle1 = createSprite(450,Math.round(random(20,350)));
     obstacle1.addImage("ob1",obstacle1Img);
-    obstacle1.scale = 0.2;
+    obstacle1.scale = 0.19;
     obstacle2 = createSprite(450,Math.round(random(20,350)));
     obstacle2.addImage("ob2",obstacle2Img);
     obstacle2.scale = 0.2;
@@ -46,7 +45,7 @@ function draw() {
          drawSprites();
          textSize(20)
          text("Dodge the Obstacles!",100,20);
-         text("USE UP AND DOWN ARROW!",80,380);
+         text("USE up and down ARROW!",80,380);
       
     
 }
